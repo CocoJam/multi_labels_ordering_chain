4094ad20245fb8dc37b689b73efe8fc4b07c6324
james <ljam763@aucklanduni.ac.nz>
2018-05-15 11:54:07 +1200

    Adjusted file

--- src/main/java/MEKA_Test_Ground/CC_test.java
+++ src/main/java/MEKA_Test_Ground/CC_test.java
@@ -22,54 +22,16 @@ import java.util.List;
 
 public class CC_test {
     public static void main(String[] args) throws Exception {
-        ConverterUtils.DataSource source = new ConverterUtils.DataSource("src/main/CAL500_clustered_blah.arff");
+        ConverterUtils.DataSource source = new ConverterUtils.DataSource("src/main/CAL500_clustered_adjusted.arff");
         Instances data2 = source.getDataSet();
         Instances data = new Instances(data2);
-//        java.util.Enumeration<Attribute> asd= data.enumerateAttributes();
-//        List<Attribute> ListLabel = new ArrayList<>();
-//        List<Attribute> ListAttr = new ArrayList<>();
-//        while(asd.hasMoreElements()){
-//           Attribute attribute =asd.nextElement();
-//           if ( attribute.type()!=0){
-//               ListLabel.add(attribute);
-//           }
-//           else{
-//               ListAttr.add(attribute);
-//           }
-//        }
-//        java.util.Enumeration<Instance> instances = data.enumerateInstances();
-//        while(instances.hasMoreElements()){
-//            instances.nextElement().
-//        }
-
-////        System.out.println(i);
-//        int[] array = Listint.stream().mapToInt(j->j).toArray();
-//        NumericToBinary numericToBinary =new NumericToBinary();
-//        numericToBinary.setAttributeIndicesArray(array);
-//        numericToBinary.setInputFormat(data);
-//        Instances blah =NumericToBinary.useFilter(data,numericToBinary);
-//
-//////        System.out.println(data);
-//        ArffSaver saver = new ArffSaver();
-//        saver.setInstances(data);
-//        saver.setFile(new File("src/main/CAL500_clustered_blah.arff"));
-//        System.out.println(blah);
         double splitRate = 66;
         int trainSize = (int) (data.numInstances() * splitRate / 100.0);
         Instances train = new Instances(data, 0, trainSize);
         Instances test = new Instances(data, trainSize, data.numInstances() - trainSize);
         System.out.println("Build CC classifier");
         CC cc = new CC();
-        SMO classifier = new SMO();
-//        System.out.println(classifier.getCapabilities());
-////        classifier.setOptions(new String[] { "-R" });
         MLUtils.prepareData(data2);
-//        System.out.println(data2);
-//        cc.setClassifier(classifier);
         cc.buildClassifier(data2);
-//        String top = "PCut1";
-//        String vop = "3";
-//        Result result = Evaluation.evaluateModel(cc, train, test, top, vop);
-//        System.out.println(result);
     }
 }
