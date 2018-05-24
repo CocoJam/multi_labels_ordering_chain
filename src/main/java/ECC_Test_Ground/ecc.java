73289fd1b53f73710711b87c2540f9f85c8598d6
Haomin Liu <karliuhaomin@hotmail.com>
2018-05-24 15:32:55 +1200

    output ecc with different cluster number

--- src/main/java/ECC_Test_Ground/ecc.java
+++ src/main/java/ECC_Test_Ground/ecc.java
@@ -1,44 +1,18 @@
 package ECC_Test_Ground;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import java.io.PrintWriter;
-
 import Prepare_CC.Cluster_CC_Builder;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-
 import meka.classifiers.multilabel.meta.BaggingML;
 import meka.core.Result;
-import weka.classifiers.functions.Logistic;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
-import weka.classifiers.functions.SMO;
-
-import Prepare_CC.CC_Util;
-import WEKA_Test_Ground.Cluster_Fliter;
-import meka.classifiers.multilabel.CC;
 import meka.classifiers.multilabel.Evaluation;
 import meka.core.MLUtils;
-import meka.core.Result;
-import meka.classifiers.multilabel.meta.BaggingML;
-
-
 
 public class ecc {
 
-    //        for i in range(10)]
 
-    public static BaggingML EccTest( String file, String fileNum) throws Exception {
+    public static BaggingML EccTest( String file, int clusterNum) throws Exception {
         // 8 clusters
 //        for (int i = 0; i < 8; i++) {
 //            Cluster_CC_Builder cluster_cc_builder = new Cluster_CC_Builder("src/main/CAL500_clustered_adjusted.arff",0,0);
@@ -69,7 +43,7 @@ public class ecc {
 //        Instances D = loadInstances("Music.arff");
 //        Instances D_train = new Instances(data,0,400);
 //        Instances D_test = new Instances(data,400,data.numInstances()-400);
-//         Train ECC
+//        Train ECC
 //        A Bagging ensemble of I chains, bagging P % of the instances
 //        default I = 10 P = 1
 //        BaggingML h = new BaggingML();
@@ -85,13 +59,13 @@ public class ecc {
         h.globalInfo();
         ConverterUtils.DataSource source = new ConverterUtils.DataSource(file);
         Instances dataset = source.getDataSet();
-        Cluster_CC_Builder cluster_cc_builder= new Cluster_CC_Builder(0,dataset,0.0);
+        Cluster_CC_Builder cluster_cc_builder= new Cluster_CC_Builder(clusterNum,dataset,0.0);
         Instances data = cluster_cc_builder.parsedCluster;
         MLUtils.prepareData(data);
         Result result = Evaluation.cvModel(h,data,10,"PCut1","3");
 
         //Evaluation metrics
-        double hamming_loss = Double.parseDouble(result.getMeasurement("Hamming score").toString());
+        double hamming_loss = Double.parseDouble(result.getMeasurement("Hamming loss").toString());
         double exact_match = Double.parseDouble(result.getMeasurement("Exact match").toString());
         double accuracy = Double.parseDouble(result.getMeasurement("Accuracy").toString());
         double averaging = ((1 - hamming_loss) + exact_match + accuracy) / 3;
@@ -99,7 +73,7 @@ public class ecc {
         System.out.println(result);
         System.out.println("== Additional Measurements (Evaluation Metrics Averaging)");
         System.out.println("averaging:" + averaging);
-        PrintWriter writer = new PrintWriter("result"+ fileNum + ".txt", "UTF-8");
+        PrintWriter writer = new PrintWriter("result"+ clusterNum + ".csv", "UTF-8");
         writer.println(result);
         writer.println("== Additional Measurements (Evaluation Metrics)");
         writer.println("hamming_loss:" + hamming_loss);
@@ -130,8 +104,8 @@ public class ecc {
         //-p Size of each bag, as a percentage of total training size (default 67)
         //-W <classifier name>
         //Full name of base classifier. (default: meka.classifiers.multilabel.CC)
-        for (int i=0;i<10;i++){
-            ecc.EccTest("src/main/CAL500_clustered_adjusted.arff", Integer.toString(i));
+        for (int i=0;i<8;i++){
+            ecc.EccTest("src/main/CAL500_clustered_adjusted.arff", i);
         }
     }
 }
